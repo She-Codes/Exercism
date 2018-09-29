3a8776a7a4dabf3f7ba8fd87c0f5f633485f9c05
She-Codes <n.y.shaw@gmail.com>
2018-09-29 00:05:11 -0400

    second iteration of leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,10 +1,3 @@
 export function isLeap(year) {
-  // if it's evenly divisible by 100, but not evenly divisible by 400
-  if ( (year % 100 === 0) && (year % 400 !== 0) ) {
-    return false;
-  }
-  // if it's divisible by 4 (or 400)
-  if ( year % 4 === 0 ) {
-    return true;
-  }
+  return (((year % 100 === 0) && (year % 400 !== 0)) || year % 4 !== 0 ) ? false : true;
 }
